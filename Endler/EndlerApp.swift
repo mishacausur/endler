b0aa0db79f1bf5aa0341c1b52dbf185cd439e525
Misha Causur <mishacausur@mail.ru>
2023-04-27 23:29:35 +0500

    Initial Commit

---
+++ Endler/EndlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndlerApp.swift
+//  Endler
+//
+//  Created by Misha Causur on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EndlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
